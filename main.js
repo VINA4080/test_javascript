f379cae5be274127d3b29bffb7326dbf65e7c680
VINA4080 <vinaandrianarisoa1@gmail.com>
2022-08-19 07:58:04 +0300

    test javascript

---
+++ main.js
@@ -0,0 +1,23 @@
+var nombre = [2,6,3,4,9,10];
+
+var tableau2 = [];
+
+for (let i = 0; i < nombre.length; i++) {
+    tableau2[i] = nombre[i] * 2;
+}
+
+// console.log(nombre.length);
+console.log(tableau2);
+console.log(showlast(tableau2));
+
+function showlast(tableau2){
+    for (let i = 0; i < tableau2.length; i++){
+        
+    }
+
+    console.log(tableau2[i]);
+
+    return tableau2;
+}
+
+
